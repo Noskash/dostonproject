4d02ea0ced008c0d2db0f20d8423b19d2c49e971
Noskash <noskashik@gmail.com>
2025-07-03 21:34:45 +0500

    commit

---
+++ db/db.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+func Connect_to_database() (*sql.DB, error) {
+	err := godotenv.Load("dostonproject/.env")
+	if err != nil {
+		log.Fatal("Ошибка при загрузке .env файла")
+	}
+	Db_Name := os.Getenv("DB_NAME")
+	Port := os.Getenv("PORT")
+	SSLMODE := os.Getenv("SSLMODE")
+	Password := os.Getenv("PASSWORD")
+	User := os.Getenv("User")
+	connectStr := fmt.Sprintf("postgres://%s:%s@localhost:%s/%s?sslmode=%s", User, Password, Port, Db_Name, SSLMODE)
+	db, err := sql.Open("postgres", connectStr)
+	if err != nil {
+		log.Fatal("Ошибка подключения к базе данных")
+	}
+	return db, nil
+}
